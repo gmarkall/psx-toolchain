1f319097bf800444b3ffe1ac713129df4725a2ae
YusufKhan-improves-shit <98565009+YusufKhan-improves-shit@users.noreply.github.com>
2022-04-05 17:01:32 -0700

    Replace ssh with https

--- clone-all.sh
+++ clone-all.sh
@@ -33,13 +33,13 @@ topdir=$(cd $(dirname $0)/..;pwd)
 cd ${topdir}
 
 # Toolchain
-git clone -b psx git@github.com:gmarkall/binutils-gdb.git
-git clone -b psx git@github.com:gmarkall/gcc.git
-git clone -b psx git@github.com:gmarkall/newlib.git
-git clone -b grm-newlib-toolchain git@github.com:gmarkall/PSn00bSDK.git
+git clone -b psx https://github.com/gmarkall/binutils-gdb.git
+git clone -b psx https://github.com/gmarkall/gcc.git
+git clone -b psx https://github.com/gmarkall/newlib.git
+git clone -b grm-newlib-toolchain https://github.com/Lameguy64/PSn00bSDK.git
 
 # Bristol / Embecosm Embedded Benchmark Suite (BEEBS)
-#git clone -b grm-size-wip git@github.com:embecosm/riscv-beebs.git beebs
+#git clone -b grm-size-wip https://github.com:embecosm/riscv-beebs.git beebs
 
 # Emulator
 #git clone https://git.qemu.org/git/qemu.git
